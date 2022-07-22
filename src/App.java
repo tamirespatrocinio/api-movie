144eae41936494faac058a3ce5f5d25f37345308
Tamires Patrocinio <tamirespatrocinio.ti@gmail.com>
2022-07-22 14:56:09 -0300

    first commit

---
+++ src/App.java
@@ -0,0 +1,35 @@
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        // fazer conexão HTTP e buscar
+        String url = "https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060";
+        URI address = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(address).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+        System.out.println(body);
+
+        // extrair dados específicos
+
+        var parser = new JsonParser();
+        List<Map<String, String>> MovieList = parser.parse(body);
+
+        // exibir dados específicos
+        for (Map<String, String> movie : MovieList) {
+            System.out.println(movie.get("title"));
+            System.out.println(movie.get("image"));
+            System.out.println(movie.get("imDbRating"));
+            System.out.println();
+
+        }
+    }
+}
